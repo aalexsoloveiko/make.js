96d1f0676261c50ca55edeba1e54b390ba4777e7
aalexsoloveiko <43556007+aalexsoloveiko@users.noreply.github.com>
2018-10-28 15:39:39 +0300

    Add files via upload

--- make.js
+++ make.js
@@ -1,4 +1,30 @@
-function sumOfOther(arr) {
-  let sumAll = arr.reduce((sum, current) => sum + current);
-  return arr.map((val, i) => sumAll - val);
+////Функция 1 sumOfOther
+//
+//function sumOfOther(arr) {
+//  let sumAll = arr.reduce((sum, current) => sum + current);
+//  return arr.map((val, i) => sumAll - val);
+//}
+
+
+//Функция 2 
+
+function make() {
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  function total() {
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return total;
+  }
+  total.toString = function () { return sum };
+
+  return total;
 }
+console.log(make(15)(34, 21, 666)(41));
+
+
+
+
